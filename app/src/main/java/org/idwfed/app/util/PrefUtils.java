48c65e0b49b69106344a576c129e0055edc2aca3
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2014-09-20 20:56:01 +0800

    more prefs to be stored

--- app/src/main/java/org/idwfed/app/util/PrefUtils.java
+++ app/src/main/java/org/idwfed/app/util/PrefUtils.java
@@ -8,24 +8,34 @@ import android.content.SharedPreferences;
  */
 public class PrefUtils {
 
+
+
     public static SharedPreferences getPreferences(Context context) {
         return context.getSharedPreferences("org.idwfed.app", Context.MODE_PRIVATE);
     }
 
-    public void setUsername(Context context, String username){
+    public static void setUsername(Context context, String username){
         getPreferences(context).edit().putString("idwf.app.username", username).commit();
     }
 
-    public String getUsername(Context context){
+    public static String getUsername(Context context){
         return getPreferences(context).getString("idwf.app.username", "");
     }
 
-    public void setServerUrl(Context context, String serverUrl){
+    public static void setServerUrl(Context context, String serverUrl){
         getPreferences(context).edit().putString("idwf.app.serverurl", serverUrl).commit();
     }
 
-    public String getServerUrl(Context context){
+    public static String getServerUrl(Context context){
         return getPreferences(context).getString("idwf.app.serverurl", "");
+    }
+
+    public static void setPassword(Context context, String password){
+        getPreferences(context).edit().putString("idwf.app.password", password).commit();
+    }
+
+    public static String getPassword(Context context){
+        return getPreferences(context).getString("idwf.app.password", "");
     }
 
 }
